d8a85c20ca3d595e6b2d434ed5d5fbf28cead038
AbdelI <homeworkoracle@protonmail.com>
2020-04-27 13:44:00 +0200

    PrivateRoute.js

---
+++ room_game/src/utils/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...restOfProps }) => {
+  return (
+    <Route
+      {...restOfProps}
+      render={() => {
+        if (localStorage.getItem("token")) {
+          return <Component />;
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
